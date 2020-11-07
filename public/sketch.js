e3464cb95ccd405c9c20f59093b17f0fbbf8402e
JWJ van der Poel <jor@hack-pro.fritz.box>
2020-11-07 13:39:20 +0100

    trying to send data to sketch.js

--- public/sketch.js
+++ public/sketch.js
@@ -1,25 +1,24 @@
+// var socket = require("socket.io");
+// Use import vs. Require
 
-//var socket = require('socket.io');
-//socket = io();
-
-
+var socket = io();
 let width = 600;
 let height = 400;
 
-// let ball = {
-//   x: 300,
-//   y: 200,
-//   xspeed: 4,
-//   yspeed: -3
-// }
+//Write Function that receives Messages
 
 function setup() {
-    createCanvas(width, height);
-    // stroke(255);
-    frameRate(30);
-    socket = io.connect('http://localhost:3000')
+  createCanvas(width, height);
+  // stroke(255);
+  frameRate(30);
+  socket = io.connect("http://localhost:3000");
+  socket.on("connection", simpleFunction);
 
-    ball = new Ball();
+  ball = new Ball();
+}
+
+function simpleFunction(bla) {
+  console.log(`Something received in Sketch`);
 }
 
 let y = 100;
@@ -29,44 +28,46 @@ let color = 0;
 let ball;
 
 function draw() {
-    background(0);
-    ball.move()
-    ball.bounce()
-    ball.show()
+  background(0);
+  ball.move();
+  ball.bounce();
+  ball.show();
 
-    sendData()
+  // sendData();
 }
 
+//Probably don't need this ( I just need to receive messages (data), not send)
 function sendData() {
-    socket.emit('func1', ball.y);
-    socket.emit('func2', ball.x);
-    console.log(ball.x);  //Check in browser console
+  socket.emit("func1", ball.y);
+  socket.emit("func2", ball.x);
+  //   console.log(ball.x); //Check in browser console
 }
 
+// console.log(oscMessage);
 
 class Ball {
-    constructor() {
-        this.x = 300;
-        this.y = 200;
-        this.xspeed = 4;
-        this.yspeed = -3;
+  constructor() {
+    this.x = 300;
+    this.y = 200;
+    this.xspeed = 4;
+    this.yspeed = -3;
+  }
+  move() {
+    this.x = this.x + this.xspeed;
+    this.y = this.y + this.yspeed;
+  }
+  bounce() {
+    if (this.x > width || this.x < 0) {
+      this.xspeed = this.xspeed * -1;
     }
-    move() {
-        this.x = this.x + this.xspeed;
-        this.y = this.y + this.yspeed;
+    if (this.y > height || this.y < 0) {
+      this.yspeed = this.yspeed * -1;
     }
-    bounce() {
-        if (this.x > width || this.x < 0) {
-            this.xspeed = this.xspeed * -1;
-        }
-        if (this.y > height || this.y < 0) {
-            this.yspeed = this.yspeed * -1;
-        }
-    }
-    show() {
-        stroke(255);
-        strokeWeight(5);
-        noFill()
-        ellipse(this.x, this.y, 24, 24);
-    }
+  }
+  show() {
+    stroke(255);
+    strokeWeight(5);
+    noFill();
+    ellipse(this.x, this.y, 24, 24);
+  }
 }
--- server.js
+++ server.js
@@ -1,87 +1,103 @@
-
 //Import Express and set to variable (which is also a function call)
-var express = require('express');
+var express = require("express");
 // Create the App
 var app = express();
-var osc = require("osc");
-
 // Listen to port 3000
 var server = app.listen(3000);
+// Use the public folder
 
-// Use the public folder 
-app.use(express.static('public'));
-
-console.log('============ Server Running ============');
-
-var socket = require('socket.io');
+app.use(express.static("public"));
+var socket = require("socket.io");
 var io = socket(server);
-io.sockets.on('connection', newConnection);
+io.sockets.on("connection", sendToSketch);
 
-//This function gets triggered when there's a new socket connection from the client
-function newConnection(socket) {
-    console.log('new connection' + socket.id);
+console.log("============ Server Running ============");
 
-}
-
-/// NOW RECEIVE FROM OM
 var osc = require("osc");
 // Instantiate a new OSC Serial Port.
 var serialPort = new osc.SerialPort({
-    devicePath: process.argv[2] || "/dev/tty.usbmodem221361"
+  devicePath: process.argv[2] || "/dev/tty.usbmodem221361",
 });
 
-serialPort.on("message", function (oscMessage) {
-    console.log(oscMessage);
-});
-
-// Open the port.
 serialPort.open();
-
 
 /****************
  * OSC Over UDP *
  ****************/
-
 var getIPAddresses = function () {
-    var os = require("os"),
-        interfaces = os.networkInterfaces(),
-        ipAddresses = [];
+  var os = require("os"),
+    interfaces = os.networkInterfaces(),
+    ipAddresses = [];
 
-    for (var deviceName in interfaces) {
-        var addresses = interfaces[deviceName];
-        for (var i = 0; i < addresses.length; i++) {
-            var addressInfo = addresses[i];
-            if (addressInfo.family === "IPv4" && !addressInfo.internal) {
-                ipAddresses.push(addressInfo.address);
-            }
-        }
+  for (var deviceName in interfaces) {
+    var addresses = interfaces[deviceName];
+    for (var i = 0; i < addresses.length; i++) {
+      var addressInfo = addresses[i];
+      if (addressInfo.family === "IPv4" && !addressInfo.internal) {
+        ipAddresses.push(addressInfo.address);
+      }
     }
-
-    return ipAddresses;
+  }
+  return ipAddresses;
 };
 
 var udpPort = new osc.UDPPort({
-    localAddress: "0.0.0.0",
-    localPort: 57121
+  localAddress: "0.0.0.0",
+  localPort: 57121,
 });
 
 udpPort.on("ready", function () {
-    var ipAddresses = getIPAddresses();
+  var ipAddresses = getIPAddresses();
 
-    console.log("Listening for OSC over UDP.");
-    ipAddresses.forEach(function (address) {
-        console.log(" Host:", address + ", Port:", udpPort.options.localPort);
-    });
+  console.log("Listening for OSC over UDP.");
+  ipAddresses.forEach(function (address) {
+    //Here we see the address from Opusmodus
+    console.log(" Host:", address + ", Port:", udpPort.options.localPort);
+  });
 });
 
-udpPort.on("message", function (oscMessage) {
-    var globalVar = { oscMessage };
-    console.log(oscMessage);
+//This is new
+udpPort.on("message", function (socket, oscMessage) {
+  sendToSketch(socket, oscMessage);
 });
 
 udpPort.on("error", function (err) {
-    console.log(err);
+  console.log(err);
 });
 
 udpPort.open();
 
+function sendToSketch(socket) {
+  // console.log(socket);
+  // console.log(oscMessage); //undefined
+  let bla = "Test String";
+  console.log(`This gets triggered`);
+  // console.log(socket.emit);
+  socket.broadcast.emit("connection", bla);
+  // socket.broadcast.emit("fromOM", oscMessage);
+  // console.log(data);
+}
+
+function sendData() {
+  socket.emit("func1", ball.y);
+  socket.emit("func2", ball.x);
+  //   console.log(ball.x); //Check in browser console
+}
+// OM Sends to Server
+// Server Receives the Message and Launches a function
+// Function sends the Message to Sketch (socket.emit)
+function newConnection(socket) {
+  console.log("new connection ID: " + socket.id);
+  //If there's a message called 'mouse', trigger the mouseMsg function
+  // socket.on("mouse", mouseMsg);
+  socket.on("test", oscMessage);
+
+  function oscMessage(data) {
+    //broadcast.emit sends the message back out
+    // socket.broadcast.emit("mouse", data);
+    // socket.broadcast.emit("test", oscMessage);
+    //The io.sockets refers to everything, so it will also send the message back to the sender
+    //io.sockets.emit('mouse', data);
+    console.log(data);
+  }
+}
